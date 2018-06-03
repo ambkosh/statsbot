554f56d84dfce9a34639da2b9b1dc7f6fd9f5c35
ambkosh <ajcrizon@gmail.com>
2018-06-03 22:02:42 +0200

    stack graph works now. need to figure out how to make it prettier still

--- botmodules/make_graph.py
+++ botmodules/make_graph.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3.6
 
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
+import numpy as np
 
-from sqlalchemy import func, text, cast, Time
+from sqlalchemy import func, text, cast, Time, and_
 
 from botmodules.sqlconnect import make_connection, Submissions, Comments
 from botmodules.upload_image import upload_image
@@ -157,5 +159,141 @@ def total_distribution_graph(table, time, session):
     #plt.yscale('log')
     plt.show()
     return(plt)
+
+
+
+
+class Flair_lists(object):
+    """creates the score lists for each flair"""
+
+
+    result_dict = {}
+    marked = {}
+
+    def __init__(self, flair, score, datum, unique_flairs):
+        self.flair = flair
+        self.score = score
+        self.date = datum
+        self.uniques = unique_flairs
+
+    def add_value(self, value, flair):
+        """adds values to dictionary"""
+
+        try:
+            current_list = self.result_dict[flair]
+            current_list.append(value)
+            self.result_dict.update({flair: current_list})
+        except KeyError:
+            current_list = [value]
+            self.result_dict.update({flair: current_list})
+
+        return(self.result_dict)
+
+
+    def mark(self):
+        """mark value as added for that day"""
+
+        try:
+            current_list = self.marked[self.date]
+            current_list.append(self.flair)
+            self.marked.update({self.date:current_list})
+        except KeyError:
+            current_list = [self.flair]
+            self.marked.update({self.date:current_list})
+
+
+    def check_mark(self, old_date, last_date):
+        """check if flair is marked for that day, if not add zero to list"""
+
+        if old_date != self.date: #new date, check which flairs are not yet marked
+            for flair in self.uniques:
+                if not flair in self.marked[old_date]: #if true flair is not in marked flairs for time in old_date
+                    self.add_value(0, flair)
+
+        if last_date == "last":
+            for flair in self.uniques:
+                if not flair in self.marked[self.date]: #if true flair is not in marked flairs for time in old_date
+                    self.add_value(0, flair)
+
+
+
+def scatter_graph_total(session, date):
+
+
+
+    result = session.query(func.sum(Submissions.num_komments).label("score"), func.date_trunc('week', Submissions.datum).label("datum"), Submissions.flair)\
+        .filter(and_(Submissions.datum >= date))\
+        .group_by(func.date_trunc('week', Submissions.datum), Submissions.flair)\
+        .order_by(func.date_trunc('week', Submissions.datum), func.sum(Submissions.score).desc())\
+        .having(func.count(Submissions.id) >= 0)
+
+    flairs = []
+    dates = []
+    last_date = ""
+    old_date = result.first().datum
+
+
+    for item in result:
+        flairs.append(item.flair)
+        try:
+            if dates[-1] != item.datum:
+                dates.append(item.datum)
+        except IndexError:
+            dates.append(item.datum)
+
+    unique_flairs = list(set(flairs))
+
+    last = result[-1]
+
+    for item in result:
+
+        flair_list = Flair_lists(item.flair, item.score, item.datum, unique_flairs)
+
+        flair_list.mark() # mark flair for that date
+        flair_list.add_value(item.score, item.flair) #add value for flair
+
+        if item == last:
+            flair_list.check_mark(old_date, "last")
+        else:
+            flair_list.check_mark(old_date, "")
+
+        old_date = item.datum
+
+    result_dict = Flair_lists("","","","").result_dict
+
+    y_list = []
+    for keys, values in result_dict.items():
+        y_list.append(values)
+
+    #
+    # create graph
+    #
+
+    ticks = mdates.DateFormatter('%Y-%m')
+
+    cmap = plt.get_cmap('terrain')
+    color = cmap(np.linspace(0, 1, (len(result_dict.keys()))))
+
+    #color_patches = []
+    # for i in range(0, len(unique_flairs)):
+    #     color_patch = mpatches.Patch(color=color[i], label=unique_flairs[i])
+    #     color_patches.append(color_patch)
+
+    plt.style.use('ggplot')
+    plt.rcParams.update({'figure.autolayout': True})
+    plt.figure(figsize=(15,5))
+
+    print(result_dict.keys())
+
+
+    ax = plt.subplot()
+    ax.stackplot(dates, y_list, labels=result_dict.keys(), colors=color)
+    ax.legend(loc=2, prop={'size': 7}, ncol=6)
+
+    ax.xaxis.set_major_formatter(ticks)
+    ax.xaxis.set_tick_params(labelrotation=55)
+    #plt.legend(handles=color_patches)
+
+    plt.show()
 
 
